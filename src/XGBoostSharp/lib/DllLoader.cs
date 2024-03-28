37f61ed0b190d7e1566e4a9eddcfccdc51cb8898
mdabros <mdabros@users.noreply.github.com>
2024-03-28 21:09:21 +0100

    Add support for loading linux and osx native libs

--- src/XGBoostSharp/lib/DllLoader.cs
+++ src/XGBoostSharp/lib/DllLoader.cs
@@ -5,6 +5,23 @@ namespace XGBoostSharp.lib;
 
 public static class DllLoader
 {
+    public static void LoadNativeLibrary()
+    {
+        var libraryPath = GetLibraryPath();
+        LoadLibrary(libraryPath);
+    }
+
     [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
-    public static extern IntPtr LoadLibrary(string lpFileName);
+    static extern IntPtr LoadLibrary(string lpFileName);
+
+    static string GetLibraryPath()
+    {
+        return RuntimeInformation.OSDescription switch
+        {
+            var os when os.Contains("Windows") => @"runtimes\win-x64\native\xgboost.dll",
+            var os when os.Contains("Linux") => @"runtimes/linux-x64/native/libxgboost.so",
+            var os when os.Contains("Darwin") => @"runtimes/osx-x64/native/libxgboost.dylib",
+            _ => throw new PlatformNotSupportedException("x64 for windows, Linux, and OSX is supported")
+        };
+    }
 }
