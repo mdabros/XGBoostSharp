4b32828ffb658064566454acbb3e09a240f3c2d1
Mads Dabros <mdabros@users.noreply.github.com>
2024-04-26 21:05:34 +0200

    CI: Enable CI for osx-x64 and fix solution debug configuration (#9)
    This PR adds the following:
     - Add reference to libxgboost-2.0.3 osx-x64 for the test project.
     - Delete DllLoader and use DllImport only for native calls.
    - Add macos-13 to pipeline os matrix. Macos-13 image is x64, while
    macos-14 and macos-latest is arm64.
    See:
    [https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners/about-github-hosted-runners](https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners/about-github-hosted-runners)
     - Fix solution debug configuration.

--- src/XGBoostSharp/lib/XGBoostSharp_NATIVE_METHODS.cs
+++ src/XGBoostSharp/lib/XGBoostSharp_NATIVE_METHODS.cs
@@ -3,64 +3,76 @@ using System.Runtime.InteropServices;
 
 namespace XGBoostSharp.lib;
 
-// Relevant links:
+// Relevant links XGBoost:
 // https://github.com/dmlc/xgboost/blob/7a6121669097745f57b8aaad1dd3a162fef96612/jvm-packages/xgboost4j/src/main/java/ml/dmlc/xgboost4j/java/XGBoostJNI.java#L105
 // https://github.com/dmlc/xgboost/blob/7a6121669097745f57b8aaad1dd3a162fef96612/src/c_api/c_api.cc#L895
 // https://xgboost.readthedocs.io/en/stable/tutorials/c_api_tutorial.html
 
+// Relevant links for cross-platform DllImport:
+// https://docs.microsoft.com/en-us/dotnet/standard/native-interop/pinvoke
+// https://docs.microsoft.com/en-us/dotnet/standard/native-interop/best-practices
+// https://github.com/dotnet/runtime/issues/8295
+// https://github.com/MonoGame/MonoGame/issues/5339#issuecomment-353751447
+// https://github.com/dotnet/coreclr/pull/19373
+// https://learn.microsoft.com/en-us/samples/dotnet/samples/dllmap-demo/
+// https://learn.microsoft.com/en-us/dotnet/standard/native-interop/native-library-loading
+
 public static class XGBOOST_NATIVE_METHODS
 {
-    const string DllLocation = @"runtimes\win-x64\native\xgboost.dll";
+    // We can use `xgboost` as dll name for all platforms
+    // since .NET handles the platform-specific naming
+    // and file extension (e.g., .dll, .so, .dylib)
+    // when searching for the dll.
+    // See https://learn.microsoft.com/en-us/dotnet/standard/native-interop/native-library-loading.
+    const string XGBoostNtvDllName = "xgboost";
 
-    static XGBOOST_NATIVE_METHODS() { DllLoader.LoadLibrary(DllLocation); }
-
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern string XGBGetLastError();
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGDMatrixCreateFromMat(float[] data, ulong nrow, ulong ncol,
                                                     float missing, out IntPtr handle);
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGDMatrixFree(IntPtr handle);
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGDMatrixGetFloatInfo(IntPtr handle, string field,
                                                    out ulong len, out IntPtr result);
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGDMatrixSetFloatInfo(IntPtr handle, string field,
                                                    float[] array, ulong len);
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGBoosterCreate(IntPtr[] dmats,
                                              ulong len, out IntPtr handle);
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGBoosterFree(IntPtr handle);
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGBoosterSetParam(IntPtr handle, string name, string val);
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGBoosterUpdateOneIter(IntPtr bHandle, int iter,
                                                     IntPtr dHandle);
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGBoosterPredict(IntPtr bHandle, IntPtr dHandle,
                                               int optionMask, int ntreeLimit,
                                               int training, // Only relevant for DART training. See https://github.com/dmlc/xgboost/issues/5601.
                                               out ulong predsLen, out IntPtr predsPtr);
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGBoosterSaveModel(IntPtr bHandle, string fileName);
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGBoosterLoadModel(IntPtr bHandle, string fileName);
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGDMatrixCreateFromFile(string fname, int silent, out IntPtr DMtrxHandle);
 
-    [DllImport(DllLocation)]
+    [DllImport(XGBoostNtvDllName)]
     public static extern int XGBoosterDumpModel(IntPtr handle, string fmap, int with_stats, out int out_len, out IntPtr dumpStr);
 }
