ba880ee8def817bb15bd876b99bc860edd46bb37
mdabros <m_dabros@hotmail.com>
2024-08-18 16:49:06 +0200

    Add and use parameter names

--- src/XGBoostSharp/lib/Booster.cs
+++ src/XGBoostSharp/lib/Booster.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Runtime.InteropServices;
+using static XGBoostSharp.lib.ParameterNames;
 
 namespace XGBoostSharp.lib;
 
@@ -73,39 +74,39 @@ public class Booster : IDisposable
         // support internationalisation i.e. support floats with commas (e.g. 0,5F)
         var nfi = new NumberFormatInfo { NumberDecimalSeparator = "." };
 
-        SetParameter("max_depth", ((int)parameters["max_depth"]).ToString());
-        SetParameter("learning_rate", ((float)parameters["learning_rate"]).ToString(nfi));
-        SetParameter("n_estimators", ((int)parameters["n_estimators"]).ToString());
-        SetParameter("silent", ((bool)parameters["silent"]).ToString());
-        SetParameter("objective", (string)parameters["objective"]);
-        SetParameter("booster", (string)parameters["booster"]);
-        SetParameter("tree_method", (string)parameters["tree_method"]);
+        SetParameter(max_depth, ((int)parameters[max_depth]).ToString());
+        SetParameter(learning_rate, ((float)parameters[learning_rate]).ToString(nfi));
+        SetParameter(n_estimators, ((int)parameters[n_estimators]).ToString());
+        SetParameter(silent, ((bool)parameters[silent]).ToString());
+        SetParameter(objective, (string)parameters[objective]);
+        SetParameter(booster, (string)parameters[booster]);
+        SetParameter(tree_method, (string)parameters[tree_method]);
 
-        SetParameter("nthread", ((int)parameters["nthread"]).ToString());
-        SetParameter("gamma", ((float)parameters["gamma"]).ToString(nfi));
-        SetParameter("min_child_weight", ((int)parameters["min_child_weight"]).ToString());
-        SetParameter("max_delta_step", ((int)parameters["max_delta_step"]).ToString());
-        SetParameter("subsample", ((float)parameters["subsample"]).ToString(nfi));
-        SetParameter("colsample_bytree", ((float)parameters["colsample_bytree"]).ToString(nfi));
-        SetParameter("colsample_bylevel", ((float)parameters["colsample_bylevel"]).ToString(nfi));
-        SetParameter("reg_alpha", ((float)parameters["reg_alpha"]).ToString(nfi));
-        SetParameter("reg_lambda", ((float)parameters["reg_lambda"]).ToString(nfi));
-        SetParameter("scale_pos_weight", ((float)parameters["scale_pos_weight"]).ToString(nfi));
+        SetParameter(nthread, ((int)parameters[nthread]).ToString());
+        SetParameter(gamma, ((float)parameters[gamma]).ToString(nfi));
+        SetParameter(min_child_weight, ((int)parameters[min_child_weight]).ToString());
+        SetParameter(max_delta_step, ((int)parameters[max_delta_step]).ToString());
+        SetParameter(subsample, ((float)parameters[subsample]).ToString(nfi));
+        SetParameter(colsample_bytree, ((float)parameters[colsample_bytree]).ToString(nfi));
+        SetParameter(colsample_bylevel, ((float)parameters[colsample_bylevel]).ToString(nfi));
+        SetParameter(reg_alpha, ((float)parameters[reg_alpha]).ToString(nfi));
+        SetParameter(reg_lambda, ((float)parameters[reg_lambda]).ToString(nfi));
+        SetParameter(scale_pos_weight, ((float)parameters[scale_pos_weight]).ToString(nfi));
 
-        SetParameter("base_score", ((float)parameters["base_score"]).ToString(nfi));
-        SetParameter("seed", ((int)parameters["seed"]).ToString());
-        SetParameter("missing", ((float)parameters["missing"]).ToString(nfi));
+        SetParameter(base_score, ((float)parameters[base_score]).ToString(nfi));
+        SetParameter(seed, ((int)parameters[seed]).ToString());
+        SetParameter(missing, ((float)parameters[missing]).ToString(nfi));
 
-        SetParameter("sample_type", (string)parameters["sample_type"]);
-        SetParameter("normalize_type ", (string)parameters["normalize_type"]);
-        SetParameter("rate_drop", ((float)parameters["rate_drop"]).ToString(nfi));
-        SetParameter("one_drop", ((int)parameters["one_drop"]).ToString());
-        SetParameter("skip_drop", ((float)parameters["skip_drop"]).ToString(nfi));
+        SetParameter(sample_type, (string)parameters[sample_type]);
+        SetParameter(normalize_type, (string)parameters[normalize_type]);
+        SetParameter(rate_drop, ((float)parameters[rate_drop]).ToString(nfi));
+        SetParameter(one_drop, ((int)parameters[one_drop]).ToString());
+        SetParameter(skip_drop, ((float)parameters[skip_drop]).ToString(nfi));
 
-        if (parameters.TryGetValue("num_class", out var value))
+        if (parameters.TryGetValue(num_class, out var value))
         {
             m_numClass = (int)value;
-            SetParameter("num_class", m_numClass.ToString());
+            SetParameter(num_class, m_numClass.ToString());
         }
     }
 
@@ -123,34 +124,34 @@ public class Booster : IDisposable
 
     public static void PrintParameters(IDictionary<string, object> parameters)
     {
-        Console.WriteLine("max_depth: " + (int)parameters["max_depth"]);
-        Console.WriteLine("learning_rate: " + (float)parameters["learning_rate"]);
-        Console.WriteLine("n_estimators: " + (int)parameters["n_estimators"]);
-        Console.WriteLine("silent: " + (bool)parameters["silent"]);
-        Console.WriteLine("objective: " + (string)parameters["objective"]);
-        Console.WriteLine("booster: " + (string)parameters["booster"]);
-        Console.WriteLine("tree_method: " + (string)parameters["tree_method"]);
+        Console.WriteLine($"{nameof(max_depth)}: {(int)parameters[max_depth]}");
+        Console.WriteLine($"{nameof(learning_rate)}: {(float)parameters[learning_rate]}");
+        Console.WriteLine($"{nameof(n_estimators)}: {(int)parameters[n_estimators]}");
+        Console.WriteLine($"{nameof(silent)}: {(bool)parameters[silent]}");
+        Console.WriteLine($"{nameof(objective)}: {(string)parameters[objective]}");
+        Console.WriteLine($"{nameof(booster)}: {(string)parameters[booster]}");
+        Console.WriteLine($"{nameof(tree_method)}: {(string)parameters[tree_method]}");
 
-        Console.WriteLine("nthread: " + (int)parameters["nthread"]);
-        Console.WriteLine("gamma: " + (float)parameters["gamma"]);
-        Console.WriteLine("min_child_weight: " + (int)parameters["min_child_weight"]);
-        Console.WriteLine("max_delta_step: " + (int)parameters["max_delta_step"]);
-        Console.WriteLine("subsample: " + (float)parameters["subsample"]);
-        Console.WriteLine("colsample_bytree: " + (float)parameters["colsample_bytree"]);
-        Console.WriteLine("colsample_bylevel: " + (float)parameters["colsample_bylevel"]);
-        Console.WriteLine("reg_alpha: " + (float)parameters["reg_alpha"]);
-        Console.WriteLine("reg_lambda: " + (float)parameters["reg_lambda"]);
-        Console.WriteLine("scale_pos_weight: " + (float)parameters["scale_pos_weight"]);
+        Console.WriteLine($"{nameof(nthread)}: {(int)parameters[nthread]}");
+        Console.WriteLine($"{nameof(gamma)}: {(float)parameters[gamma]}");
+        Console.WriteLine($"{nameof(min_child_weight)}: {(int)parameters[min_child_weight]}");
+        Console.WriteLine($"{nameof(max_delta_step)}: {(int)parameters[max_delta_step]}");
+        Console.WriteLine($"{nameof(subsample)}: {(float)parameters[subsample]}");
+        Console.WriteLine($"{nameof(colsample_bytree)}: {(float)parameters[colsample_bytree]}");
+        Console.WriteLine($"{nameof(colsample_bylevel)}: {(float)parameters[colsample_bylevel]}");
+        Console.WriteLine($"{nameof(reg_alpha)}: {(float)parameters[reg_alpha]}");
+        Console.WriteLine($"{nameof(reg_lambda)}: {(float)parameters[reg_lambda]}");
+        Console.WriteLine($"{nameof(scale_pos_weight)}: {(float)parameters[scale_pos_weight]}");
 
-        Console.WriteLine("base_score: " + (float)parameters["base_score"]);
-        Console.WriteLine("seed: " + (int)parameters["seed"]);
-        Console.WriteLine("missing: " + (float)parameters["missing"]);
+        Console.WriteLine($"{nameof(base_score)}: {(float)parameters[base_score]}");
+        Console.WriteLine($"{nameof(seed)}: {(int)parameters[seed]}");
+        Console.WriteLine($"{nameof(missing)}: {(float)parameters[missing]}");
 
-        Console.WriteLine("sample_type: " + ((float)parameters["sample_type"]));
-        Console.WriteLine("normalize_type: " + ((float)parameters["normalize_type"]));
-        Console.WriteLine("rate_drop: ", +((float)parameters["rate_drop"]));
-        Console.WriteLine("one_drop: ", +((int)parameters["one_drop"]));
-        Console.WriteLine("skip_drop: ", +((float)parameters["skip_drop"]));
+        Console.WriteLine($"{nameof(sample_type)}: {(float)parameters[sample_type]}");
+        Console.WriteLine($"{nameof(normalize_type)}: {(float)parameters[normalize_type]}");
+        Console.WriteLine($"{nameof(rate_drop)}: {(float)parameters[rate_drop]}");
+        Console.WriteLine($"{nameof(one_drop)}: {(int)parameters[one_drop]}");
+        Console.WriteLine($"{nameof(skip_drop)}: {(float)parameters[skip_drop]}");
     }
 
     public void SetParameter(string name, string val)
---
+++ src/XGBoostSharp/lib/ParameterNames.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XGBoostSharp.lib;
+
+public static class ParameterNames
+{
+    public const string max_depth = "max_depth";
+    public const string learning_rate = "learning_rate";
+    public const string n_estimators = "n_estimators";
+    public const string silent = "silent";
+    public const string objective = "objective";
+    public const string booster = "booster";
+    public const string tree_method = "tree_method";
+    public const string nthread = "nthread";
+    public const string gamma = "gamma";
+    public const string min_child_weight = "min_child_weight";
+    public const string max_delta_step = "max_delta_step";
+    public const string subsample = "subsample";
+    public const string colsample_bytree = "colsample_bytree";
+    public const string colsample_bylevel = "colsample_bylevel";
+    public const string reg_alpha = "reg_alpha";
+    public const string reg_lambda = "reg_lambda";
+    public const string scale_pos_weight = "scale_pos_weight";
+    public const string base_score = "base_score";
+    public const string seed = "seed";
+    public const string missing = "missing";
+    public const string sample_type = "sample_type";
+    public const string normalize_type = "normalize_type";
+    public const string rate_drop = "rate_drop";
+    public const string one_drop = "one_drop";
+    public const string skip_drop = "skip_drop";
+    public const string num_class = "num_class";
+}
