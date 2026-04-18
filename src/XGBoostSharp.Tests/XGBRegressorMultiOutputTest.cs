385d13b36947072b58d6ac54b482c92a4ed14234
mdabros <m_dabros@hotmail.com>
2026-04-18 21:41:58 +0200

    use actual

--- src/XGBoostSharp.Tests/XGBRegressorMultiOutputTest.cs
+++ src/XGBoostSharp.Tests/XGBRegressorMultiOutputTest.cs
@@ -9,7 +9,6 @@ namespace XGBoostSharp.Test;
 public class XGBRegressorMultiOutputTest
 {
     const string TEST_FILE = "tmpfile_regressor_multioutput.json";
-    const int NOutputs = 2;
 
     [TestInitialize, TestCleanup]
     public void Reset()
@@ -21,7 +20,7 @@ public class XGBRegressorMultiOutputTest
     }
 
     [TestMethod]
-    public void XGBRegressorTest_PredictMultiOutput_ShapeIsCorrect()
+    public void XGBRegressorTest_PredictMultiOutput()
     {
         var dataTrain = TestUtils.DataTrainMultiOutput;
         var labelsTrain = TestUtils.LabelsTrainMultiOutputRegression;
@@ -29,7 +28,8 @@ public class XGBRegressorMultiOutputTest
         using var sut = CreateSut();
         sut.Fit(dataTrain, labelsTrain);
 
-        var actual = sut.PredictMultiOutput(dataTrain);
+        using var dMatrixTest = new DMatrix(dataTrain);
+        var actual = sut.PredictMultiOutput(dMatrixTest);
         var expected = TestUtils.ExpectedMultiOutputRegressionPredictions;
 
         TestUtils.AssertAreEqual(expected, actual);
@@ -53,42 +53,6 @@ public class XGBRegressorMultiOutputTest
     }
 
     [TestMethod]
-    public void XGBRegressorTest_PredictMultiOutput_SaveAndLoad()
-    {
-        var dataTrain = TestUtils.DataTrainMultiOutput;
-        var labelsTrain = TestUtils.LabelsTrainMultiOutputRegression;
-
-        using var sut = CreateSut();
-        sut.Fit(dataTrain, labelsTrain);
-        var expected = sut.PredictMultiOutput(dataTrain);
-        sut.SaveModelToFile(TEST_FILE);
-
-        var sutLoaded = XGBRegressor.LoadFromFile(TEST_FILE);
-        var actual = sutLoaded.PredictMultiOutput(dataTrain);
-
-        TestUtils.AssertAreEqual(expected, actual);
-    }
-
-    [TestMethod]
-    [DataRow(ModelFormat.Json)]
-    [DataRow(ModelFormat.Ubj)]
-    public void XGBRegressorTest_PredictMultiOutput_SaveAndLoadRaw(string format)
-    {
-        var dataTrain = TestUtils.DataTrainMultiOutput;
-        var labelsTrain = TestUtils.LabelsTrainMultiOutputRegression;
-
-        using var sut = CreateSut();
-        sut.Fit(dataTrain, labelsTrain);
-        var expected = sut.PredictMultiOutput(dataTrain);
-        var savedData = sut.SaveModelToByteArray(format);
-
-        var sutLoaded = XGBRegressor.LoadFromByteArray(savedData);
-        var actual = sutLoaded.PredictMultiOutput(dataTrain);
-
-        TestUtils.AssertAreEqual(expected, actual);
-    }
-
-    [TestMethod]
     public void XGBRegressorTest_PredictMultiOutput_MultiOutputTreeStrategy()
     {
         var dataTrain = TestUtils.DataTrainMultiOutput;
@@ -100,9 +64,9 @@ public class XGBRegressorMultiOutputTest
             multiStrategy: MultiStrategy.MultiOutputTree);
         sut.Fit(dataTrain, labelsTrain);
 
-        var predictions = sut.PredictMultiOutput(dataTrain);
+        var actual = sut.PredictMultiOutput(dataTrain);
 
-        TestUtils.AssertShape(predictions, dataTrain.Length, NOutputs);
+        TestUtils.AssertAreEqual(TestUtils.ExpectedMultiOutputTreeRegressionPredictions, actual);
     }
 
     static XGBRegressor CreateSut() =>
